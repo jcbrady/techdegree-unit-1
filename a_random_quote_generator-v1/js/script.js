7a2994ed4934e6b9d44ea94072a023a27ca28049
JC <jcbrady89@yahoo.com>
2020-07-18 06:40:06 -0600

    testing 1
    Testing console.log to access array and objects.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -14,28 +14,59 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
     quote: "The truth is of course is that there is no journey. We are arriving and departing all at the same time.",
-    source: "David Bowie"
+    source: "—David Bowie",
+    year: "1980's"
   },
   {
     quote: "I never studied anything, really. I didn't study the drums. I joined bands and made all the mistakes on stage.",
-    source: "Ringo Starr"
+    source: "—Ringo Starr"
   },
   {
     quote: "Each generation wants new symbols, new people, new names. They want to divorce themselves from their predecessors.",
-    source: "Jim Morrison"
+    source: "—Jim Morrison"
   },
   {
     quote: "When the power of love overcomes the love of power, the world will know peace.",
-    source: "Jimi Hendrix"
+    source: "—Jimi Hendrix"
   },
   {
     quote: "There are two basic motivating forces: fear and love. When we are afraid, we pull back from life.",
-    source: "John Lennon"
+    source: "—John Lennon",
+    year: "1960's"
   }
 ]
 
-console.log(quotes)
+//console.log(quotes)
+//console.log(quotes[0].quote)
+//console.log(quotes[0].source)
+//console.log(Object.keys(quotes))
+//console.log(Object.values(quotes))
+//console.log(Object.keys(year))
 
+// for in loop - logs all keys and values
+// for (let propName in quotes) {
+//   console.log(propName)
+//   console.log(quotes[propName])
+// }
+
+const student = [
+  {
+    name: "Quincy",
+    grades: [60, 85, 90, 71]
+  },
+  {
+    name: "Casey",
+    grades: [70, 95, 95, 81]
+  }
+]
+
+for (i = 0; i < student.length; i++) {
+  let arr = student[i]
+  console.log(arr)
+  for (let propName in arr) {
+    console.log(`In the "for in" loop: ${arr[propName]}`)
+  }
+}
 /***
  * `getRandomQuote` function
  ***/
