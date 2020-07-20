00988781716e9516c472466092ba0029113891ed
JC <jcbrady89@yahoo.com>
2020-07-20 10:13:30 -0600

    final project
    Edited and commented.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -8,78 +8,55 @@ project 1 - A Random Quote Generator
 // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /***
- * `quotes` array
+ * `quotes` array - Stores data in an array of objects. Note that some objects
+ * have more properties than others.
  ***/
 
 const quotes = [
   {
     quote: "The truth is of course is that there is no journey. We are arriving and departing all at the same time.",
-    source: "—David Bowie",
+    source: "David Bowie",
     year: "1980's",
-    citation: "Brainyquote.com",
-    living: false
+    citation: "Brainyquote.com"
   },
   {
     quote: "I never studied anything, really. I didn't study the drums. I joined bands and made all the mistakes on stage.",
-    source: "—Ringo Starr",
+    source: "Ringo Starr",
     year: "1960's",
-    citation: "Brainyquote.com",
-    living: true
+    citation: "Brainyquote.com"
   },
   {
     quote: "Each generation wants new symbols, new people, new names. They want to divorce themselves from their predecessors.",
-    source: "—Jim Morrison",
-    living: false
+    source: "Jim Morrison"
   },
   {
     quote: "When the power of love overcomes the love of power, the world will know peace.",
-    source: "—Jimi Hendrix",
-    living: false
+    source: "Jimi Hendrix"
   },
   {
     quote: "There are two basic motivating forces: fear and love. When we are afraid, we pull back from life.",
-    source: "—John Lennon",
-    year: "1960's",
-    living: false
+    source: "John Lennon",
+    year: "1960's"
   }
 ]
 
-// outputs the raw array and objects to the console
-//console.log(quotes)
-
-// SOME DIFFERENT WAYS OF LOOPING THROUGH THE ARRAY OF OBJECTS:
-// outputs text
-// for (i = 0; i < quotes.length; i++) {
-//   let arr = quotes[i]
-//   for (let propName in arr) {
-//     console.log(arr[propName])
-//   }
-// }
-
-// outputs {objects}
-// for (let propName in quotes) {
-//   console.log(quotes[propName])
-// }
-
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function - Generates a random number and returns a random
+ * object from the quotes array.
  ***/
 
 function getRandomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length)
   let randomQuote = quotes[randomNumber]
-  // console.log("randomNumber inside getRandomQuote function: " + randomNumber)
-  // console.log("randomQuote: " + randomQuote.quote + " " + randomQuote.source)
-  // console.log({ quote: randomQuote })
-
-  // console.log(randomQuote)
-
   return randomQuote
 }
 
 /***
- * `printQuote` function
+ * `printQuote` function - calls the getRandomQuote() function and puts the
+ * return results in a variable. Builds an html object and uses condiional logic
+ * based on if the array contains the ressults.
  ***/
+
 function printQuote() {
   let rando = getRandomQuote()
   let html = `
@@ -87,29 +64,20 @@ function printQuote() {
   <p class="source">${rando.source}
   `
   if (rando.citation) {
-    console.log("citation")
-    //html +=   `<span class="citation"> quote citation </span>
-    // <span class="year"> quote year </span>
-    //</p>`
+    html += `<span class="citation">${rando.citation}</span>`
   }
   if (rando.year) {
-    console.log("year")
-  }
-  if (rando.living) {
-    console.log("yes, they are still alive!")
-  } else {
-    console.log("They have passed away.")
+    html += `<span class="year">${rando.year}</span>`
   }
 
-  console.log(rando)
-  console.log(html)
+  html += "</p>"
+  document.getElementById("quote-box").innerHTML = html
 }
 
 /***
- * click event listener for the print quote button
+ * click event listener for the print quote button - Listens for user interaction button click
+ * to add a new quote to the viewable html page
  * DO NOT CHANGE THE CODE BELOW!!
  ***/
 
 document.getElementById("load-quote").addEventListener("click", printQuote, false)
-
-//console.log(getRandomQuote())
